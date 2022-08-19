ced947971ea3f2befa1592edccc2967c94cdc56e
chrilona <christabelalona@gmail>
2022-08-19 11:43:29 +0300

    second committ

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ function loadJSONproducts(){
 
     .then(data => {
         for(var i=0; i<data.length; i++){
-            document.getElementById("products").innerHTML += data[i].id + data[i].name + " " +data[i].category}
+            document.getElementById("products").innerHTML += data[i].name +" :" +data[i].category
+        }
+
         console.log(data)
-        // document.querySelector("#products").innerText = data
-        document.querySelector("#fruList").innerText = data
+        
+        document.querySelector("#fruList").innerText = data.category = "fruits"
+        // document.querySelector("#fruList").innerText = data
         document.querySelector("#vegList").innerText = data
     })
     .catch(error => {
@@ -15,3 +18,4 @@ function loadJSONproducts(){
     })
 }
 loadJSONproducts()
+
