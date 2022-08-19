f3a1ff6f722dcabd9055214b57ff3306344acbc3
chrilona <christabelalona@gmail>
2022-08-19 09:55:52 +0300

    second commit

--- index.js
+++ index.js
@@ -1,97 +1,17 @@
-//server.js
-// var http = require('http'); // Import Node.js core module
-// // var GreensKiosk = require('./products');
-// var server = http.createServer(function (req, res) {   // create web server
-//   // Set CORS headers
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Request-Method', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-//   res.setHeader('Access-Control-Allow-Headers', '*');
-//   if (req.method === 'OPTIONS') {
-//     res.writeHead(200);
-//     res.end();
-//     return;
-//   }
-//   if (req.url == '/') { // check the URL of the current request
-//     // set response header
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     // set response content
-//     res.write('<html><body><p>Greens Kiosk API.</p></body></html>');
-//     res.end();
-//   }
-//   else if (req.url == "/products") { // check the URL of the current request
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.write(JSON.stringify(GreensKiosk.getItems()));
-//     res.end();
-//   }
-//   else if (req.url == "/products/fruits") {
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.write(JSON.stringify(GreensKiosk.getItems('fruits')));
-//     res.end();
-//   }
-//   else if (req.url == '/products/vegetables') {
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.write(JSON.stringify(GreensKiosk.getItems('vegetables')));
-//     res.end();
-//   }
-//   else
-//     res.end('Invalid Request!');
-// });
-// server.listen(5000); // listen for any incoming requests
-// console.log('Node.js web server at port 5000 is running..')
-
-
-// GreensKiosk.js
-
-
-// function getItems(category) {
-// if (category) {
-//     let filteredProducts = products.filter(item => {
-//         return item.category === category;
-//     });
-//     return filteredProducts;
-// }
-// return products;
-// }
-// GreensKiosk = {
-// getItems: getItems
-// };
-
-// module.exports = GreensKiosk;
-// const productList = document.querySelector('.productItem');
-
-// function loadJSONproducts(){
-//     fetch('products.json')
-//     .then(response => response.json())
-//     .then(data =>{
-//         let html = '';
-//         data.forEach(product => {
-//             html += `
-//                 <div class = "productItem">
-//                         <ul id = "fruList">${product.name}</ul>
-//                         <ul id = "vegList">${product.name}</ul>
-//                         <ul id = "products">${product.name}</ul>
-                      
-//                 </div>
-//             `;
-//         });
-//         productList.innerHTML = html;
-//     })
-//     .catch(error => {
-//         alert(`User live server or local server`);
-//     })
-// }
-
-// loadJSONproducts()
-
 function loadJSONproducts(){
     fetch('products.json')
     .then(response => response.json())
+
     .then(data => {
+        for(var i=0; i<data.length; i++){
+            document.getElementById("products").innerHTML += data[i].id + data[i].name + " " +data[i].category}
         console.log(data)
-        document.querySelector("#products").innerText = data
+        // document.querySelector("#products").innerText = data
         document.querySelector("#fruList").innerText = data
         document.querySelector("#vegList").innerText = data
+    })
+    .catch(error => {
+        alert(`An error occured`);
     })
 }
 loadJSONproducts()
